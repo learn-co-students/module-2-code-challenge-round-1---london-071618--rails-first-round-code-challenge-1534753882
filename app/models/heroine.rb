786a4e968ed1fb13804858c7ffe43a88099759c4
Flatiron School <flatiron@Flatirons-MacBook-Air.local>
2018-08-20 10:36:47 +0100

    Jo

--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,14 @@
 class Heroine < ApplicationRecord
+  belongs_to :power
+
+  validates :super_name, presence: true, uniqueness: true, length: { minimum: 2 }
+
+  def self.power(power_name)
+    if power_name != ""
+      Heroine.all.select {|heroine| heroine.power.name == power_name}
+    else
+      Heroine.all
+    end
+  end
+
 end
